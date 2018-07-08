4300be6527d97d59b12d5437e07cfeb21fa230f8
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-07-08 15:34:58 -0700

    Switch to Nix for CI

---
+++ nix/typed-spreadsheet.nix
@@ -0,0 +1,18 @@
+{ mkDerivation, async, base, diagrams-cairo, diagrams-gtk
+, diagrams-lib, foldl, gtk, microlens, stdenv, stm, text
+, transformers
+}:
+mkDerivation {
+  pname = "typed-spreadsheet";
+  version = "1.1.3";
+  src = ./..;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    async base diagrams-cairo diagrams-gtk diagrams-lib foldl gtk
+    microlens stm text transformers
+  ];
+  executableHaskellDepends = [ base diagrams-lib text ];
+  description = "Typed and composable spreadsheets";
+  license = stdenv.lib.licenses.bsd3;
+}
